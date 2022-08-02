550b4c7bfc9d9b24941dde181ca95b8be54c7576
Sandhya Shekar <sandhya1902.ingmail.com>
2022-08-01 22:33:56 -0700

    Java code

---
+++ Fabric-Java/src/main/java/Asset.java
@@ -0,0 +1,19 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
+
+public class Asset {
+    @JsonProperty("ID")
+    public String id;
+    @JsonProperty("Owner")
+    public String owner;
+    @JsonProperty("Name")
+    public String name;
+    @JsonProperty("Region")
+    public String region;
+    @JsonProperty("IPFSHash")
+    public String iPFSHash;
+    @JsonProperty("AuthorizedDevices")
+    public ArrayList<String> authorizedDevices;
+    @JsonProperty("AuthorizedUsers")
+    public ArrayList<String> authorizedUsers;
+}
---
+++ Fabric-Java/src/main/java/TestClass.java
@@ -0,0 +1,7 @@
+public class TestClass {
+
+    public static void main() throws Exception {
+        FabricClass fc1 = new FabricClass();
+        System.out.println("Result  " + fc1.pushIPFSHashToFabric("device3", "krjdhf8345hjl"));
+    }
+}
