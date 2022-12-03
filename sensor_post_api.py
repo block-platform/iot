b9bf6b13887ac80dd6d8e9667807aae6e0d7bf3e
Rounak Salim <rounaksalim@gmail.com>
2022-12-02 17:01:53 -0800

    Update sensor_post_api.py

--- sensor_post_api.py
+++ sensor_post_api.py
@@ -7,6 +7,7 @@ from datetime import datetime
 projectId = "2C3ti7yXc1nkKH6WNlNrrT13VKF"
 projectSecret = "0e905e5ebd6c5aa010bb00fdb2ee8869"
 endpoint = "https://ipfs.infura.io:5001"
+endpoint_aws = "http://34.193.203.14:8080/iot/ipfs-hash/"
 
 # Get I2C bus
 bus = smbus.SMBus(1)
@@ -23,50 +24,53 @@ bus.write_byte_data(0x18, 0x08, 0x03)
 
 time.sleep(0.5)
 
-# MCP9808 address, 0x18(24)
-# Read data back from 0x05(5), 2 bytes
-# Temp MSB, TEMP LSB
-data = bus.read_i2c_block_data(0x18, 0x05, 2)
+while(1):
+    
+    # MCP9808 address, 0x18(24)
+    # Read data back from 0x05(5), 2 bytes
+    # Temp MSB, TEMP LSB
+    data = bus.read_i2c_block_data(0x18, 0x05, 2)
 
-# Convert the data to 13-bits
-ctemp = ((data[0] & 0x1F) * 256) + data[1]
-if ctemp > 4095:
-    ctemp -= 8192
-ctemp = ctemp * 0.0625
-ftemp = ctemp * 1.8 + 32
+    # Convert the data to 13-bits
+    ctemp = ((data[0] & 0x1F) * 256) + data[1]
+    if ctemp > 4095 :
+        ctemp -= 8192
+    ctemp = ctemp * 0.0625
+    ftemp = ctemp * 1.8 + 32
 
-# Output data to screen
-# string = "Temperature in Celsius is " + str(ctemp)
-#print("Temperature in Celsius is    : %.2f C" +  str(ctemp))
-#print("Temperature in Fahrenheit is : %.2f F" +  str(ftemp))
+    # Output data to screen
+    string = "Temperature in Celsius is " + str(ctemp)
+    #print("Temperature in Celsius is    : %.2f C" +  str(ctemp))
+    #print("Temperature in Fahrenheit is : %.2f F" +  str(ftemp))
 
-data = {
-    "temperature": ctemp,
-    "timestamp": datetime.now().timestamp(),
-}
+    data = {
+        "temperature": ctemp,
+        "timestamp": datetime.now().timestamp(),
+    }
 
 
-files = {
-    'file': json.dumps(data)
-}
+    files = {
+        'file': json.dumps(data)
+    }
 
-response = requests.post(endpoint + '/api/v0/add',
-                         files=files, auth=(projectId, projectSecret))
-p = response.json()
-hash = p['Hash']
-print(hash)
+    response = requests.post(endpoint + '/api/v0/add',
+                            files=files, auth=(projectId, projectSecret))
+    p = response.json()
+    hash = p['Hash']
+    print(hash)
 
-hash = str(hash)
+    hash = str(hash)
 
-parameters = {
-    'device_id': 'dev-1',
-    'ipfs_hash': [hash],
-    'device_key': 'OGqwsyc8u8'
-}
+    parameters = {
+        'device_id' : 'dev-1',
+        'ipfs_hash' : [hash],
+        'api_key': 'OGqwsyc8u8'
+    }
 
-response = requests.put(
-    "http://34.193.203.14:8080/iot/ipfs-hash/", json=parameters)
-print(response)
-data = response.text
-json = json.loads(data)
-print(json)
+    response = requests.put(endpoint_aws, json = parameters)
+    print(response)
+    data = response.text
+    json = json.loads(data)
+    print(json)
+    
+    time.sleep(30)
